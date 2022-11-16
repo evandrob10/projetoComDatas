42f44e23d62b3abd3f14d2584cd9e4ed6746ef63
Evandro Barros <evandrobarrosb98@gmail.com>
2022-11-16 17:58:50 -0300

    incremento da function de cadastro dos endereços

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,21 +1,59 @@
 package Application;
-
-import java.time.Duration;
 // importação de pacotes:
-import java.time.LocalDate;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 //importação de Class
 import Entites.PersonDate;
+import Entites.Endereco;
 
 public class Main {
+	
 	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
 		LocalDateTime dataNascimento = LocalDateTime.parse("1996-03-13T14:30");
 		LocalDateTime data_atual = LocalDateTime.parse("2022-11-13T20:30");
 		PersonDate pessoa = new PersonDate("Evandro",20,dataNascimento);
 		
+		end
 		
-		System.out.println(pessoa.getIdade());
+	}
+	public void cadastrarEndereco(PersonDate pessoa) {
+		Scanner input = new Scanner(System.in);
 		
+		String rua, numeroCasa, bairro, cidade, estado, complemento;
+		
+		System.out.println("Numero de endereço que deseja cadastrar: ");
+		int numeroEndereco = input.nextInt();
+		input.nextLine();
+		
+		Endereco[] endereco =  new Endereco [numeroEndereco];
+		
+		do {
+			System.out.println("Digite a rua: ");
+			rua = input.nextLine();
+			System.out.println("Digite o numero casa: ");
+			numeroCasa = input.next();
+			System.out.println("Digite o bairro: ");
+			bairro = input.nextLine();
+			System.out.println("Digite a cidade: ");
+			cidade = input.nextLine();
+			System.out.println("Digite o estado: ");
+			estado = input.nextLine();
+			System.out.println("(Opcional) Digite o complemento: ");
+			complemento = input.nextLine();
+
+			
+			
+			--numeroEndereco;
+		}while(numeroEndereco != 0);
+		
+		input.close();
 	}
 }
